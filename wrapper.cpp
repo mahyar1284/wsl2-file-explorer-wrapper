f1fec6122e5b11f8d02851de11b20b067279195f
Mahyar Shokraeian <mahyarshokraeian@live.com>
2021-03-09 23:19:00 +0330

    code readability improvements

--- wrapper.cpp
+++ wrapper.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main (int argc, char* argv[]) 
 {
-    std::string cmd(argv[argc - 1]); 
-    for(int n = 0; n < cmd.length(); n++)
-        if(cmd.at(n) == '/')
-            cmd.replace(n, 1, "\\\\");
-    system(std::string("explorer.exe " + cmd).c_str());
+    string cmd(argv[argc - 1]); 
+    for(auto var : cmd)
+            try{cmd.replace(cmd.find('/'), 1, "\\\\");}catch(exception){}
+    system(string("explorer.exe " + cmd).c_str());
     printf("\n");
     return 0;
 }
